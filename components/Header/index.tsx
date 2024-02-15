6312f4f3f754d7fcd2156c75b06a83a4dd960abc
OlegKipchatov <kipchatovbrend@yandex.ru>
2024-02-15 13:52:58 +0300

    Init user dropdown menu

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,28 +1,24 @@
 import Link from "next/link";
 import { cookies } from "next/headers";
+import { User, UserMetadata } from "@supabase/supabase-js";
 import { createClient } from "@/utils/supabase/server";
-import { getUserData, isAuthUser } from "@/utils/supabase/requests";
-import AuthButton from "@/components/AuthButton";
+import { getUserData, getUserMetadata, isAuthUser } from "@/utils/supabase/requests";
+import AuthButton from "@/components/Header/AuthButton";
 
 export default async function Header() {
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
-    const user = await getUserData(supabase);
     const isAuth = await isAuthUser(supabase);
+    const user = await getUserData(supabase);
+    const userMetadata = await getUserMetadata(supabase);
 
     return(
         <nav className="w-full flex fixed backdrop-blur-xl justify-center border-b border-b-foreground/10 h-16 z-50">
             <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-                { user?.email && 
-                    <Link
-                        href="/settings"
-                        className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
-                    >
-                        {user.email}
-                    </Link>
+                { isAuth 
+                    ? <AuthButton user={user as User} userMetadata={userMetadata as UserMetadata} />
+                    : <Link href="/login" className="ml-auto py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">Login</Link>
                 }
-
-                <AuthButton isAuth={isAuth} />
             </div>
         </nav>
     );
