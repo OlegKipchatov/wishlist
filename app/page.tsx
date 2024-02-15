8269e3dbcc233e3b9338ba0eb346b062a48f473b
OlegKipchatov <kipchatovbrend@yandex.ru>
2024-02-16 00:15:46 +0300

    Improve display list user

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import { cookies } from "next/headers";
-import { createClient } from "@/utils/supabase/server";
-import { getListUsers, isAuthUser } from "@/utils/supabase/requests";
 import { redirect } from "next/navigation";
 import Link from "next/link";
+import { createClient } from "@/utils/supabase/server";
+import { getListUsers, isAuthUser } from "@/utils/supabase/requests";
+import CardAvatar from "@/components/CardAvatar";
 
 export default async function Index() {
   const cookieStore = cookies();
@@ -17,13 +18,15 @@ export default async function Index() {
 
   return (
     <div className="max-w-2xl w-full pt-20 p-6 flex-1 flex flex-col gap-4 items-center">
-      <ul>
+      <ul className="space-y-4">
         { listUsers?.map((user) => {
             const linkToUserList = `/list/${user.login}`;
+            const displayName = `${user.first_name} ${user.last_name}`;
             return(
               <li key={user.login}>
-                <Link href={linkToUserList} className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
-                  {user.first_name} {user.last_name}
+                <Link href={linkToUserList} className="py-2 px-3 flex items-center gap-2 text-xl rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+                  <CardAvatar name={displayName} />
+                  {displayName}
                 </Link>
               </li>
             );
