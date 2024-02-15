6645bc47a9b3cecd366a1d9c07b478543acb6a5e
OlegKipchatov <kipchatovbrend@yandex.ru>
2024-02-15 18:08:37 +0300

    Update footer link

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({
             <p>
               Powered by{" "}
               <a
-                href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+                href="https://vk.com/jarponok"
                 target="_blank"
                 className="font-bold hover:underline"
                 rel="noreferrer"
