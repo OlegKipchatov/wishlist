b53f0f91c97b67c0d7eb83ead18e234e2ae59dbd
OlegKipchatov <kipchatovbrend@yandex.ru>
2024-02-15 23:43:28 +0300

    Improve display image

--- components/Card/CardImage.tsx
+++ components/Card/CardImage.tsx
@@ -1,13 +1,11 @@
 'use client';
 
-import { createClient } from "./../../utils/supabase/client";
 import { useEffect, useState } from "react";
+import { createClient } from "./../../utils/supabase/client";
 
 type Props = {
     imageName?: string,
 };
-
-const mobileStyle = 'self-center rounded-full h-32 w-32';
 
 export default function CardImage(props: Props) {
     const { imageName } = props;
@@ -26,8 +24,8 @@ export default function CardImage(props: Props) {
     return(
         <>
             {imageURL
-                ? <div className="flex justify-center max-w-96 sm:max-w-80">
-                    <img src={imageURL} className="rounded-t-lg sm:rounded-tl-lg sm:rounded-tr-none" alt="Product item image" />
+                ? <div className="flex justify-center sm:justify-start max-w-96 w-full flex-1/3">
+                    <img src={imageURL} className="rounded-t-lg sm:rounded-tl-lg sm:rounded-tr-none object-contain" alt="Product item image" />
                 </div>
             : <></>}
         </>
