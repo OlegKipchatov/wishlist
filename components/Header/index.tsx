a4e0bfcb396bfa1b2c9f44755803d502dfbcd402
OlegKipchatov <kipchatovbrend@yandex.ru>
2024-02-18 18:59:59 +0300

    Support switch theme

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,6 +4,7 @@ import { createClient } from "@/supabase/server";
 import { supabaseWorker } from '@/supabase/requests';
 import AuthButton from "@/components/Header/AuthButton";
 import { IUser } from "@/supabase/types";
+import ThemeSwitcher from "./ThemeSwticher";
 
 export default async function Header() {
     const supabase = supabaseWorker(createClient(cookies()));
@@ -12,10 +13,12 @@ export default async function Header() {
 
     return(
         <nav className="w-full flex fixed backdrop-blur-xl justify-center border-b border-b-foreground/10 h-16 z-50">
-            <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+            <div className="w-full max-w-4xl flex gap-4 justify-between items-center p-3 text-sm">
                 <Link href='/' className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                     <span>WishList</span>
                 </Link>
+
+                <ThemeSwitcher />
                 { isAuthenticated 
                     ? <AuthButton user={user as IUser} />
                     : <Link href="/login" className="ml-auto py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">Login</Link>
