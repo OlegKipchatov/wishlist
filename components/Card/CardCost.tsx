3d41651712992bdb7a0a4137098bf2c8ee0b27c7
OlegKipchatov <kipchatovbrend@yandex.ru>
2024-02-18 21:52:08 +0300

    Support display locale format item cost

--- components/Card/CardCost.tsx
+++ components/Card/CardCost.tsx
@@ -2,10 +2,16 @@ type Props = {
     cost: Number | undefined,
 }
 
+const numberFormat = Intl.NumberFormat(undefined, {style: 'currency', currency: 'RUB', maximumFractionDigits: 0});
+
 export default function CardCost(props: Props) {
     const { cost } = props;
 
     return(
-        <>{ cost ? <span className="text-xl tracking-tight text-gray-900 dark:text-white">{`Price: ${cost}₽`}</span> : <></> }</>
+        <> {
+            cost
+                ? <span className="text-xl tracking-tight text-gray-900 dark:text-white">{numberFormat.format(cost as number)}</span>
+                : <></> 
+        } </>
     );
 }
