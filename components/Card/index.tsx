b9cba0c0fe255740ab5a2079417446cb61b3ed81
OlegKipchatov <kipchatovbrend@yandex.ru>
2024-02-20 23:20:03 +0300

    Improve display card

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -9,13 +9,15 @@ interface Props {
     isCurrentUser: boolean,
 }
 
+const cardStyles = 'bg-gray-0 dark:bg-gray-100 hover:bg-gray-100 dark:hover:bg-gray-200 active:bg-gray-200 dark:active:bg-gray-300';
+
 export default function Card(props: Props) {
     const { item, isCurrentUser } = props;
     const date = new Date(item.time);
 
     return(
-        <div className='flex flex-col justify-between w-full rounded-lg shadow-xl h-auto bg-gray-0 border border-gray-100'>
-            <a href={item.link ?? '#'} target="blank" className="flex flex-col sm:flex-row btn-neutral btn-focus rounded-lg border-b border-gray-100">
+        <div className='flex flex-col justify-between w-full rounded-lg shadow-xl h-auto dark:bg-gray-100 border border-gray-100'>
+            <a href={item.link ?? '#'} target="blank" className={cardStyles + " flex flex-col sm:flex-row btn-focus rounded-lg border-b border-gray-100"}>
                 <CardImage imageName={item.image} />
 
                 <div className="flex flex-col justify-between p-4 space-y-4 w-full">
