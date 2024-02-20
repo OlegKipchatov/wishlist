8eb09150e482b48b7fddd117f8df586d87fec35f
OlegKipchatov <kipchatovbrend@yandex.ru>
2024-02-21 01:09:21 +0300

    Fix lint

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,6 +1,5 @@
 import { IUser } from "@/supabase/types";
 import Avatar from "./Avatar";
-import { getDisplayName } from "@/utils/users";
 
 type Props = {
     user: IUser,
@@ -8,11 +7,10 @@ type Props = {
 
 export default function UserCard(props: Props) {
     const { user } = props;
-    const displayName = getDisplayName(user);
 
     return(
         <div className="flex gap-2 justify-center items-center p-4 rounded-lg">
-            <Avatar name={user.first_name + ' ' + user.last_name} size='large'/>
+            <Avatar />
             <div className="flex flex-col">
                 <span className="text-4xl">{user.first_name + ' ' + user.last_name}</span>
                 <span className="font-bold text-2xl">{user.login}</span>
