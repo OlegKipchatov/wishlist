fbcecbee0f89aaadfebe334d1cf7bf1ae0042503
OlegKipchatov <kipchatovbrend@yandex.ru>
2024-02-25 21:44:14 +0300

    Remove obsolete code

--- components/Avatar.tsx
+++
@@ -1,7 +0,0 @@
-import UserIcon from '@heroicons/react/24/solid/UserCircleIcon';
-
-export default function Avatar() {
-    return (
-        <UserIcon className='w-32 h-32 text-gray-600 dark:text-gray-200'/>
-    );
-}
