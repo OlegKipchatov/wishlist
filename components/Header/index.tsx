7ca814a7b73b77e078307090eb18d5d6e1b1141e
OlegKipchatov <kipchatovbrend@yandex.ru>
2024-02-25 04:57:08 +0300

    Fix header links

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,7 +5,7 @@ import { supabaseWorker } from '@/supabase/requests';
 import AuthButton from "@/components/Header/AuthButton";
 import { IUser } from "@/supabase/types";
 import dynamic from "next/dynamic";
-import { Button } from '@nextui-org/react';
+import { Button, Link as NextUILink } from '@nextui-org/react';
 
 const ThemeSwitcher = dynamic(() => import('./ThemeSwticher'), { ssr: false });
 
@@ -18,15 +18,15 @@ export default async function Header() {
         <nav className="w-full flex fixed backdrop-blur-xl justify-center shadow-xl h-16 z-50">
             <div className="w-full max-w-4xl flex gap-4 justify-between items-center p-3 text-sm">
                 <Link href='/' className="btn-focus rounded-lg self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                    <span>WishList</span>
+                    WishList
                 </Link>
 
                 <ThemeSwitcher />
                 { isAuthenticated 
                     ? <AuthButton user={user as IUser} />
                     : <div className="flex gap-2">
-                        <Button href="/signin" variant='ghost'>SignIn</Button>
-                        <Button href="/signup" color='success'>SignUp</Button>
+                        <Button as={NextUILink} href="/signin" variant='ghost'>SignIn</Button>
+                        <Button as= {NextUILink} href="/signup" color='success'>SignUp</Button>
                     </div>
                 }
             </div>
