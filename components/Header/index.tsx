e6e6db6f50d995c07b4cbb36a5b52af84a412e6c
OlegKipchatov <kipchatovbrend@yandex.ru>
2024-02-25 20:12:05 +0300

    Improve display Header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,7 +5,8 @@ import { supabaseWorker } from '@/supabase/requests';
 import AuthButton from "@/components/Header/AuthButton";
 import { IUser } from "@/supabase/types";
 import dynamic from "next/dynamic";
-import { Button, Link as NextUILink } from '@nextui-org/react';
+import { Button, NavbarBrand, NavbarContent } from '@nextui-org/react';
+import { Navbar } from '@nextui-org/react';
 
 const ThemeSwitcher = dynamic(() => import('./ThemeSwticher'), { ssr: false });
 
@@ -15,21 +16,20 @@ export default async function Header() {
     const user = isAuthenticated && await supabase.users.getSessionUser();
 
     return(
-        <nav className="w-full flex fixed backdrop-blur-xl justify-center shadow-xl h-16 z-50">
-            <div className="w-full max-w-4xl flex gap-4 justify-between items-center p-3 text-sm">
-                <Link href='/' className="btn-focus rounded-lg self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                    WishList
-                </Link>
-
+        <Navbar>
+            <NavbarBrand as={Link} href='/' className="text-xl font-semibold">
+                WishList
+            </NavbarBrand>
+            <NavbarContent justify='end'>
                 <ThemeSwitcher />
                 { isAuthenticated 
                     ? <AuthButton user={user as IUser} />
                     : <div className="flex gap-2">
-                        <Button as={NextUILink} href="/signin" variant='ghost'>SignIn</Button>
-                        <Button as= {NextUILink} href="/signup" color='success'>SignUp</Button>
+                        <Button as={Link} href="/signin" variant='ghost'>SignIn</Button>
+                        <Button as= {Link} href="/signup" color='success'>SignUp</Button>
                     </div>
                 }
-            </div>
-        </nav>
+            </NavbarContent>
+        </Navbar>
     );
 }
